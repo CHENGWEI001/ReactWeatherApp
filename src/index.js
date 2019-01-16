748250331800b61eef77ffd7cad995a05d928319
EC2 Default User <ec2-user@ip-172-31-37-125.us-west-2.compute.internal>
2019-01-16 23:22:01 +0000

    in-function refactor with hooks

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []);
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  }
+  else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  }
+  else {
+    content = <Spinner message="Please accept location request" />;
+  }
+
+  return <div className="border red">{content}</div>;
+}
+
+class AppOld extends React.Component {
   state = { lat: null, errorMessage: '' };
 
   componentDidMount() {
